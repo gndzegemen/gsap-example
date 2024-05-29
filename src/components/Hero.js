1ad05a83f49c2d910eafdd7a085ba835d0675084
Egemen Gunduz <86930714+gndzegemen@users.noreply.github.com>
2024-05-29 23:33:42 +0300

    change hero

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -3,7 +3,7 @@ import React from "react";
 function Hero() {
   return (
     <header className="hero__header">
-      <h1>John Doe</h1>
+      <h1>Egemen</h1>
       <h2>Frontend Developer</h2>
     </header>
   );
